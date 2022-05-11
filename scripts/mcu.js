94e6484b998dd2a85c5b30c35844a651a6cee5f1
Nick Nguyen <35278403+Ooshie@users.noreply.github.com>
2022-05-10 23:45:38 -0500

    Update mcu.js

--- scripts/mcu.js
+++ scripts/mcu.js
@@ -1,7 +1,8 @@
-const result = document.getElementById('result');
+const result = document.getElementById('info');
+const api = 'https://www.whenisthenextmcufilm.com/api';
 
 function getMCU(){
-    fetch('https://www.whenisthenextmcufilm.com/api')
+    fetch(api)
         .then(res => res.json())
         .then(data => {
             result.innerHTML = 
@@ -13,6 +14,28 @@ function getMCU(){
                 <p>Release Date: ${data.release_date}</p>
                 <p>Days until release: ${data.days_until}</p>
             </span>
-            `
+            `;
     })
 }
+
+/* works without using <body onload="getMCU()">
+getMCU(api);
+
+function getMCU(api) {
+    fetch(api)
+        .then(res => res.json())
+        .then(data => {
+            result.innerHTML =
+                `
+            <img src='${data.poster_url}'/>
+            <span class="text">
+                <h1>${data.title} | ${data.type}</h1>
+                <p>${data.overview}</p>
+                <p>Release Date: ${data.release_date}</p>
+                <p>Days until release: ${data.days_until}</p>
+            </span>
+            `;
+            document.div.appendChild(result);
+        })
+}
+*/
